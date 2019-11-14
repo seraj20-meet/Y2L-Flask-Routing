fe2b58bd13cf71e5d1670c209180f3a48b2b6a6a
myname21-meet <myname21@meet.mit.edu>
2019-11-14 11:42:15 -0500

    sejo

--- databases.py
+++ databases.py
@@ -18,3 +18,51 @@ Product_object = Product(
 	desc=description)
 	session.add(product_object)
 	session.commit()
+
+def delete_product(their_name):
+   """
+   Delete all students with a
+   certain name from the database.
+   """
+   session.query().filter_by(
+       name=their_name).first().delete()
+   session.commit()
+
+def update_lab_status(name, price,picture_link,desc):
+   """
+   Update a student in the database, with
+   whether or not they have finished the lab
+   """
+   product = session.query(Product).filter_by(
+       name=name).first()
+   product.name = name
+   prodict.price= price
+   prod.pic_link = picture_link
+   prod.dexc = desc
+
+   session.commit()
+
+
+def query_all():
+   """
+   Print all the students
+   in the database.
+   """
+   Productssss = session.query(
+      Product).all()
+   return Products
+
+
+print(query_all())
+
+def query_by_name(their_name):
+   """
+   Find the first student
+   in the database, by their name
+   """
+   Product1 = session.query(
+       Product).filter_by(
+       name=their_name).first()
+   return Product1
+
+
